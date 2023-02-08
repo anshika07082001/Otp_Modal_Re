cf33a1c719bb11007960cfb2a3a82214c9f663ad
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2023-02-08 16:16:15 +0530

    final

--- src/components/ OtpLayout.tsx
+++ src/components/ OtpLayout.tsx
@@ -1,142 +1,214 @@
-import { Box, Modal } from '@mui/material'
-import React, { useEffect, useRef, useState } from 'react'
+import { Box, Modal } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 
-const style = {position: 'absolute',top: '50%',left: '50%',transform: 'translate(-50%, -50%)',width: 500,bgcolor: 'background.paper',boxShadow: 24,p: 2,};
+// inline css for modal
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 450,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 2,
+};
+// types are defined
+type otpprops = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  otp: number | undefined;
+  generateOtp: () => void;
+};
 
-type otpprops={
-    open:boolean;
-    setOpen:React.Dispatch<React.SetStateAction<boolean>>;
-    otp:number|undefined;
-    generateNum:()=>void
-}
+const OtpLayout = (props: otpprops) => {
+  var [sec, setSec] = useState(59);
+  var [stateObj, setStateObj] = useState({
+    border: "",
+    textColor: "",
+    msg: "",
+  });
+  var [limitDisable, setLimitDisable] = useState({ limit: 4, disabled: true });
+  var timerInterval = useRef<any>();
+  var inpRefs = useRef<any>([]);
 
-const  OtpLayout = (props:otpprops) => {
-    var [sec,setSec]=useState(10)
-    var [stateObj,setStateObj]=useState({border:'',textColor:'',msg:''})
-    var [limitDisable,setLimitDisable]=useState({limit:4,disabled:true})
-    var timerInterval=useRef<any>()
-    var inpRefs=useRef<any>([])
-
-    useEffect(()=>{
-        clearInterval(timerInterval.current)
-        timerInterval.current=setInterval(timer,1000)
-        if(sec==0)
-        setLimitDisable({limit:limitDisable.limit,disabled:false})
-    },[sec])
-
-    useEffect(()=>{
-        setTimeout(()=>inpRefs.current[0].focus(),100)
-    },[limitDisable.limit])
-
-    const timer=()=>{
-        if(sec>0){
-            sec--
-            setSec(sec)
+  // useEffect runs when second changes
+  useEffect(() => {
+    clearInterval(timerInterval.current);
+    timerInterval.current = setInterval(timer, 1000);
+    if (sec === 0 && limitDisable.limit > 0) {
+      setLimitDisable({ limit: limitDisable.limit, disabled: false });
+    }
+  }, [sec]);
+  // useEffect runs when limit changes and set to focus to input first element
+  useEffect(() => {
+    setTimeout(() => inpRefs.current[0].focus(), 100);
+  }, [limitDisable.limit]);
+  // timer function decrements the seconds value
+  const timer = () => {
+    if (sec > 0) {
+      sec--;
+      setSec(sec);
+    }
+  };
+  // function closes the modal
+  const closeModal = () => {
+    props.setOpen(false);
+  };
+  // onchange input handler
+  const inpHandler = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
+    var arr: any = [];
+    setStateObj({ border: "", textColor: "", msg: "" });
+    if (e.target.value.match(/^[0-9]{1}$/)) {
+      inpRefs.current[i].value = e.target.value;
+      if (inpRefs.current[i].value !== "" && inpRefs.current[i + 1] !== undefined) {
+        inpRefs.current[i + 1].focus();
+      }
+      var count: boolean = true;
+      inpRefs.current.map((item: any) => {
+        if (item.value === "") {
+          count = false;
         }
+      });
+      if (count) {
+        inpRefs.current.map((item: any) => {
+          arr.push(item.value);
+        });
+        if (parseInt(arr.join("")) === props.otp) {
+          setStateObj({
+            border: "successBorder",
+            textColor: "successText",
+            msg: "Matched successfully",
+          });
+          setTimeout(() => {
+            setStateObj({
+              border: "successBorder",
+              textColor: "successText",
+              msg: "",
+            });
+          }, 2000);
+          setTimeout(() => props.setOpen(false), 1000);
+        } else {
+          setStateObj({
+            border: "errorBorder",
+            textColor: "errorText",
+            msg: "Entered One-time passcode is incorrect!",
+          });
+        }
+      }
+    } else {
+      inpRefs.current[i].value = "";
     }
-
-    const closeModal=()=>{
-        props.setOpen(false)
+  };
+  // function regenerates the otp
+  const regenerateOtp = () => {
+    if (limitDisable.limit > 0) {
+      limitDisable.limit--;
+      setLimitDisable({ limit: limitDisable.limit, disabled: true });
+      props.generateOtp();
+      setSec(59);
+      setStateObj({
+        border: "",
+        textColor: "successText",
+        msg: "One-Time Passcode sent successfully!",
+      });
+      setTimeout(
+        () => setStateObj({ border: "", textColor: "successText", msg: "" }),
+        2000
+      );
     }
+    inpRefs.current.map((item: any) => {
+      item.value = "";
+    });
+  };
+  // Function deletes the value and moves the focus to previous element
+  const backspace = (e: React.KeyboardEvent<HTMLInputElement>, i: number) => {
+    if (e.key === "Backspace") {
+      if (inpRefs.current[i].value === "") {
+        if (i !== 0) {
+          inpRefs.current[i - 1].focus();
+        }
+      }
+    }
+  };
 
-    const inpHandler=(e:React.ChangeEvent<HTMLInputElement>,i:number)=>{
-        var arr:any=[]
-        setStateObj({border:'',textColor:'',msg:''})
-        if(e.target.value.match(/^[0-9]{1}$/)){
-            inpRefs.current[i].value=e.target.value
-            var ele = e.target
-            var nextSibling:any = ele.nextElementSibling
-            if(nextSibling!==null)
-            nextSibling?nextSibling.focus():ele.blur()
-            var count:boolean=true;
-            inpRefs.current.map((item:any)=>{
-                if(item.value==''){
-                    count=false;
-                }
-            })
-            if(count){
-                inpRefs.current.map((item:any)=>{
-                    arr.push(item.value)
-                })
-                if(arr.join('')==props.otp){
-                    setStateObj({border:'successBorder',textColor:'successText',msg:'Matched successfully'})
-                   setTimeout(()=>{setStateObj({border:'successBorder',textColor:'successText',msg:''})},2000) 
-                   setTimeout(()=>props.setOpen(false),1000)
-                }
-                else{
-                    setStateObj({border:'errorBorder',textColor:'errorText',msg:'otp entered is wrong'})
-                }
+  return (
+    <Modal
+      open={props.open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <div className="displayRow displayRow--modalHeading">
+          <label className="fontSize">Verify Email Address ({props.otp})</label>
+          <button onClick={closeModal} className="button button--danger">
+            X
+          </button>
+        </div>
+        <hr />
+        <p>Enter Your Code Here:</p>
+        <br />
+        <div className="displayRow">
+          {/* code renders input box dynamically */}
+          {props.otp !== undefined ? (
+            props.otp
+              .toString()
+              .split("")
+              .map((item, i) => {
+                return (
+                  <input
+                    className={`${stateObj.border} inputBox`}
+                    type="text"
+                    onKeyDown={(e) => backspace(e, i)}
+                    maxLength={1}
+                    minLength={1}
+                    onChange={(e) => inpHandler(e, i)}
+                    ref={(ref) => (inpRefs.current[i] = ref)}
+                  />
+                );
+              })
+          ) : (
+            <></>
+          )}
+          {/* Renders the loader */}
+          {stateObj.msg === "Matched successfully" ? (
+            <img
+              className="loader__img"
+              src="https://media.tenor.com/8KWBGNcD-zAAAAAC/loader.gif"
+              alt=""
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+        <label className={stateObj.textColor}>{stateObj.msg}</label>
+        <div className="displayRow">
+          {/* code renders the regenerate otp button */}
+          <button
+            className={
+              limitDisable.disabled
+                ? "button--disabled"
+                : "button button--enable"
             }
-        }
-        else{
-            inpRefs.current[i].value=''
-        }
-    } 
-
-
-    const regenerateNum=()=>{
-        if(limitDisable.limit>0){
-            limitDisable.limit--
-            setLimitDisable({limit:limitDisable.limit,disabled:true})
-            // setLimit(limit)
-            props.generateNum()
-            setSec(10)
-            // setLimitDisable({limit:limitDisable.limit,disabled:true})
-            // setDisabled(true)
-            setStateObj({border:'',textColor:'successText',msg:'One-Time Passcode sent successfully!'})
-            setTimeout(()=>setStateObj({border:'',textColor:'successText',msg:''}),2000)
-        }
-        else{
-            setStateObj({border:'',textColor:'errorText',msg:'Limit Exceed'})
-        }
-        inpRefs.current.map((item:any)=>{
-            item.value=''
-        })
-    }
-
-    const backspace =(e:React.KeyboardEvent<HTMLInputElement>,i:number)=>{
-        if(e.key=='Backspace'){
-            if(inpRefs.current[i].value==''){
-            setTimeout(()=>{
-                inpRefs.current[i-1].focus()
-            },200)
+            disabled={limitDisable.disabled}
+            onClick={regenerateOtp}
+          >
+            Resend One-time Passcode
+          </button>
+          <label
+            className={
+              limitDisable.limit === 0 ? "errorText marginLeft" : "marginLeft"
             }
-            if(inpRefs.current[i].value!==''){
-                setTimeout(()=>{
-                    inpRefs.current[i].focus()
-                },200)
-            }
-        }
-    }
-    
-    return (
-    <Modal open={props.open} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-        <Box sx={style}>
-            <div className='displayRow displayRow--modalHeading'>
-                <label className='fontSize'>Verify Email Address ({props.otp})</label>
-                <button onClick={closeModal} className='button button--danger'><i className="bi bi-x-lg"></i></button>
-            </div>
-            <hr/>
-            <p>Enter Your Code Here:</p>
-            <div className='displayRow'>
-                {props.otp!==undefined?
-                props.otp.toString().split('').map((item,i)=>{
-                    return (
-                        <input className={`${stateObj.border} inputBox`} type='text' onKeyDown={(e)=>backspace(e,i)}  maxLength={1} minLength={1} onChange={(e)=>inpHandler(e,i)} ref={(ref)=>inpRefs.current[i]=ref}/>
-                    )
-                })
-                :<></>}
-                {stateObj.msg=='Matched successfully'?<img className='loader__img' src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921' alt=''/>:<></>}
-            </div>
-            <label className={stateObj.textColor}>{stateObj.msg}</label>
-            <div className='displayRow'>
-              <button className={limitDisable.disabled?'button--disabled':'button button--enable'} disabled={limitDisable.disabled} onClick={regenerateNum}>Resend One-time Passcode</button>
-              <label className={limitDisable.limit==0?'text-danger':'text-dark'}>( {limitDisable.limit} Attempts left)</label>
-              <label className='text-danger'>00:{sec==0?'00':sec}</label>
-            </div>
-        </Box>
+          >
+            ( {limitDisable.limit} Attempts left)
+          </label>
+          {/* code renders the timer */}
+          <label className="errorText marginLeft">
+            00:{sec === 0 ? "00" : sec}
+          </label>
+        </div>
+      </Box>
     </Modal>
-    )
-}
+  );
+};
 
-export default  OtpLayout
+export default OtpLayout;
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,58 +1,75 @@
-import React, { useState } from 'react'
-import OtpLayout from './ OtpLayout'
+import React, { useState } from "react";
+import OtpLayout from "./ OtpLayout";
 
 const Register = () => {
-    var [num,setNum]=useState<any>(5)
-    var [otp,setOtp]=useState<number|undefined>()
-    var [msg,setMsg]=useState('')
-    // var [keyVal,setKeyVal]=useState({num:5,otp:})
-    var [open,setOpen]=useState<boolean>(false)
+  var [num, setNum] = useState<any>();
+  var [otp, setOtp] = useState<number | undefined>();
+  var [msg, setMsg] = useState("");
+  var [open, setOpen] = useState<boolean>(false);
 
-    const inpHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
-        if(e.target.value.match(/^[4-8]{1}$/)){
-            setMsg('')
-            setNum(e.target.value)
-        }
-        else{
-            if(e.target.value=='')
-            setNum(e.target.value)
-            setMsg('Number must be less than and equal to 8 or greater than and equal to 4')
-        }
+  // functions takes the input value from user to generate the otp
+  const inpHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.match(/^[4-8]{1}$/)) {
+      setMsg("");
+      setNum(Number(e.target.value));
+    } else {
+      setMsg("input must be filled in range of 4 to 8");
     }
+  };
 
-    const openModal=()=>{
-        generateNum()
-        if(num!=='')
-        setOpen(true)
-    }
-
-    const generateNum=()=>{
-        if(num!==''){
-            var max=''
-            var min='1'
-            for(var i=1;i<=num;i++){
-                max+=9
-                if(i!==1){
-                    min+=0
-                }
-            }
-            var OTP=Math.floor(Math.random()*(parseInt(max)-parseInt(min))+parseInt(min))
-            setOtp(OTP)
+  // function generates the random number
+  const generateOtp = () => {
+    if (num !== undefined && msg == "") {
+      var max = "";
+      var min = "1";
+      for (var i = 1; i <= num; i++) {
+        max += 9;
+        if (i !== 1) {
+          min += 0;
         }
+      }
+      var OTP = Math.floor(
+        Math.random() * (parseInt(max) - parseInt(min)) + parseInt(min)
+      );
+      setOtp(OTP);
+      setOpen(true);
+    } else {
+      setMsg("Please fill input with range 4-8");
     }
+  };
 
-    return (
-    <div className='displayColumn'>
-        <h1>OTP Validation</h1>
-        <div className='displayRow displayRow--userNum'>
-            <label>Enter the number to get otp(Range will be: 4-8 default value is 5):&nbsp;&nbsp;</label>
-            <input className='userNum__inp' maxLength={1} minLength={1} type='text' value={num} onChange={(e)=>inpHandler(e)}/>
-        </div>
-        <label className='errorText'>{msg}</label>
-        <button className='button' onClick={openModal}>Validate OTP</button>
-        {open?<OtpLayout open={open} setOpen={setOpen} otp={otp} generateNum={generateNum}/>:<></>}
+  return (
+    <div className="displayColumn">
+      <h1>OTP Validation</h1>
+      <div className="displayRow displayRow--userNum">
+        <label>
+          Enter the number to get otp(Range will be: 4-8):&nbsp;&nbsp;
+        </label>
+        <input
+          className="userNum__inp"
+          maxLength={1}
+          minLength={1}
+          type="text"
+          onChange={(e) => inpHandler(e)}
+        />
+      </div>
+      <label className="errorText">{msg}</label>
+      <button className="button" onClick={generateOtp}>
+        Validate OTP
+      </button>
+      {/* Rendering of Otp Modal Component*/}
+      {open ? (
+        <OtpLayout
+          open={open}
+          setOpen={setOpen}
+          otp={otp}
+          generateOtp={generateOtp}
+        />
+      ) : (
+        <></>
+      )}
     </div>
-    )
-}
+  );
+};
 
-export default Register
+export default Register;
